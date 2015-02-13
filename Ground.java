225c2036a34d599c05d8accbb7c9515a5c76d898
pnyberg <pernyb@kth.se>
2015-02-13 13:41:29 +0100

    more sauce

---
+++ Ground.java
@@ -0,0 +1,15 @@
+public class Ground {
+	private final int x, y;
+	public boolean occupied;
+	public String type;
+	public Cederman man;
+
+	public Ground(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+	public void place(Cederman man){
+		this.man = man;
+		this.man.setPosition(x, y);
+	}
+}
